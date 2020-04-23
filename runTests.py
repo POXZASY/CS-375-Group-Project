2634a8f344b55c269dcbca1e8f3e801fbe85e11e
Larkin Wisdom <lwisdom1@binghamton.edu>
2020-04-23 17:00:28 -0400

    folders organized

--- runTests.py
+++ runTests.py
@@ -8,11 +8,14 @@ dir1 = dir + "\\bottomup.exe"
 dir2 = dir + "\\recursivememo.exe"
 
 #os.system("date")
-
+try:
+    os.mkdir("output")
+except:
+    "Output directory already exists."
 for i in range(numtests):
-    input = "input"+str(i+1)+".txt"
-    output1 = "output_bottomup"+str(i+1)+".txt"
-    output2 = "output_recursivememo"+str(i+1)+".txt"
+    input = "input/input"+str(i+1)+".txt"
+    output1 = "output/output_bottomup"+str(i+1)+".txt"
+    output2 = "output/output_recursivememo"+str(i+1)+".txt"
 
     subprocess.call(['cmd', '/c', dir1, input, output1])
     subprocess.call(['cmd', '/c', dir2, input, output2])
